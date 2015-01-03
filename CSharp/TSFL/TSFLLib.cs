04900ee7e3f239b734ff9c6f1b6df214e53abada
BioCompDevSvcs <devsvcs@biocompsystems.com>
2015-01-02 22:37:57 -0600

    Restructured into class per, add RequiredColumns and MinimumRows

--- CSharp/TSFL/TSFLLib.cs
+++ CSharp/TSFL/TSFLLib.cs
@@ -2,86 +2,179 @@
 
 namespace TSFL
 {
-    public static class TSFLLib
+ 
+  public class SMA
+  {
+    public readonly int RequiredColumns = 1;
+    public readonly int MinimumRows = 1;
+
+    /// <summary>
+    /// Computes Simple Moving Average of specified period on full array passed
+    /// </summary>
+    /// <param name="data">The array of doubles to compute upon</param>
+    /// <param name="period">The length of the moving average</param>
+    /// <returns></returns>
+    public static double[] Compute(double[] data, int period)
     {
-      /// <summary>
-      /// Computes Simple Moving Average of specified period on full array passed
-      /// </summary>
-      /// <param name="data">The array of doubles to compute upon</param>
-      /// <param name="period">The length of the moving average</param>
-      /// <returns></returns>
-      public static double[] SMA(double[] data, int period)
-      {
-        //merely convert to extended form, call and return the result
-        return SMA(data, period, 0, data.Length);
-      }
-
-      /// <summary>
-      /// Computes Simple Moving Average of specified period between start and end row
-      /// </summary>
-      /// <param name="data">The array of doubles to compute upon</param>
-      /// <param name="period">The length of the moving average</param>
-      /// <param name="startRow">The starting row of the computation (zero based)</param>
-      /// <param name="endRow">The ending row of the computation (zero based)</param>
-      /// <returns></returns>
-      public static double[] SMA(double[] data, int period, int startRow, int endRow)
-      {
-        //Do checks
-        if (data == null)
-        {
-          throw new ArgumentException("data cannot be null");
-        }
-        if (period <= 0)
-        {
-          throw new ArgumentException("period must be greater than zero");
-        }
-        if (startRow < 0)
-        {
-          throw new ArgumentException("startRow cannot be negative");
-        }
-        if (endRow < 0)
-        {
-          throw new ArgumentException("endRow cannot be negative");
-        }
-        if (endRow < startRow)
-        {
-          throw new ArgumentException("endRow must be greater or equal to startRow");
-        }
-
-        //allocate returned result the same as the incoming array
-        var returnData = new double[data.Length];
-        //if the period is greater than the length of the data, just return array with zeros
-        if (period > data.Length) return returnData;
-
-        //Figure out where to start computing.  It isn't just as simple as using start and end row
-        //if the start row is less than the period, use the period, else the start row
-        int startPoint = startRow < period ? period : startRow;
-        
-        //if the endrow is bigger than or equal to the data length, use the data length
-        //else use the endrow + 1 as we are going one less than the endpoint: i < endPoint
-        int endPoint;
-        if (endRow >= data.Length)
-        {
-          endPoint = data.Length;
-        }
-        else
-        {
-          endPoint = endRow + 1;
-        }
-
-        //Walk through the data from start to end
-        for (int i = startPoint - 1; i < endPoint; i++)
-        {
-          //computing a moving window of size period
-          double value = 0;
-          for (int j = i; j > i - period; j += -1)
-          {
-            value += data[j];
-          }
-          returnData[i] = value / period;
-        }
-        return returnData;
-      }
+      //merely convert to extended form, call and return the result
+      return Compute(data, period, 0, data.GetUpperBound(0) + 1);
     }
+
+    /// <summary>
+    /// Computes Simple Moving Average of specified period between start and end row
+    /// </summary>
+    /// <param name="data">The array of doubles to compute upon</param>
+    /// <param name="period">The length of the moving average</param>
+    /// <param name="startRow">The starting row of the computation (zero based)</param>
+    /// <param name="endRow">The ending row of the computation (zero based)</param>
+    /// <returns></returns>
+    public static double[] Compute(double[] data, int period, int startRow, int endRow)
+    {
+      //Do checks
+      if (data == null)
+      {
+        throw new ArgumentException("data cannot be null");
+      }
+      if (period <= 0)
+      {
+        throw new ArgumentException("period must be greater than zero");
+      }
+      if (startRow < 0)
+      {
+        throw new ArgumentException("startRow cannot be negative");
+      }
+      if (endRow < 0)
+      {
+        throw new ArgumentException("endRow cannot be negative");
+      }
+      if (endRow < startRow)
+      {
+        throw new ArgumentException("endRow must be greater or equal to startRow");
+      }
+
+      //allocate returned result the same as the incoming array
+      var returnData = new double[data.GetUpperBound(0) + 1];
+      //if the period is greater than the length of the data, just return array with zeros
+      if (period > data.GetUpperBound(0) + 1) return returnData;
+
+      //Figure out where to start computing.  It isn't just as simple as using start and end row
+      //if the start row is less than the period, use the period, else the start row
+      int startPoint = startRow < period ? period : startRow;
+
+      //if the endrow is bigger than or equal to the data length, use the data length
+      //else use the endrow + 1 as we are going one less than the endpoint: i < endPoint
+      int endPoint;
+      if (endRow >= data.GetUpperBound(0) + 1)
+      {
+        endPoint = data.GetUpperBound(0) + 1;
+      }
+      else
+      {
+        endPoint = endRow + 1;
+      }
+
+      //Walk through the data from start to end
+      for (int i = startPoint - 1; i < endPoint; i++)
+      {
+        //computing a moving window of size period
+        double value = 0;
+        for (int j = i; j > i - period; j += -1)
+        {
+          value += data[j];
+        }
+        returnData[i] = value / period;
+      }
+      return returnData;
+    }
+
+    /// <summary>
+    /// Computes Simple Moving Average of specified period on a specified column of a 2D array of doubles
+    /// </summary>
+    /// <param name="data">The array of doubles to compute upon</param>
+    /// <param name="period">The length of the moving average</param>
+    /// <param name="columnID">The ending row of the computation (zero based)</param>
+    /// <returns></returns>
+    public static double[] Compute(double[,] data, int period, int columnID)
+    {
+      return Compute(data, period, 0, data.GetUpperBound(0) + 1, columnID);
+    }
+
+    /// <summary>
+    /// Computes Simple Moving Average of specified period between start and end row on a specified column of a 2D array of doubles
+    /// </summary>
+    /// <param name="data">The array of doubles to compute upon</param>
+    /// <param name="period">The length of the moving average</param>
+    /// <param name="startRow">The starting row of the computation (zero based)</param>
+    /// <param name="endRow">The ending row of the computation (zero based)</param>
+    /// <param name="columnID">The ending row of the computation (zero based)</param>
+    /// <returns></returns>
+    public static double[] Compute(double[,] data, int period, int startRow, int endRow, int columnID)
+    {
+      //Do checks
+      if (data == null)
+      {
+        throw new ArgumentException("data cannot be null");
+      }
+      if (period < 0)
+      {
+        throw new ArgumentException("period cannot be negative");
+      }
+      if (startRow < 0)
+      {
+        throw new ArgumentException("startRow cannot be negative");
+      }
+      if (endRow < 0)
+      {
+        throw new ArgumentException("endRow cannot be negative");
+      }
+      if (endRow < startRow)
+      {
+        throw new ArgumentException("endRow must be greater or equal to startRow");
+      }
+      if (columnID <= 0)
+      {
+        throw new ArgumentException("columnID must be greater than zero");
+      }
+      if (columnID > data.GetUpperBound(1))
+      {
+        throw new ArgumentException("columnID is beyond the bounds of the array");
+      }
+
+      //allocate returned result the same as the incoming array
+      var returnData = new double[data.GetUpperBound(0) + 1];
+      //if the period is greater than the length of the data, just return array with zeros
+      if (period > data.GetUpperBound(0) + 1) return returnData;
+
+      //Figure out where to start computing.  It isn't just as simple as using start and end row
+      //if the start row is less than the period, use the period, else the start row
+      int startPoint = startRow < period ? period : startRow;
+
+      //if the endrow is bigger than or equal to the data length, use the data length
+      //else use the endrow + 1 as we are going one less than the endpoint: i < endPoint
+      int endPoint;
+      if (endRow >= data.GetUpperBound(0) + 1)
+      {
+        endPoint = data.GetUpperBound(0) + 1;
+      }
+      else
+      {
+        endPoint = endRow + 1;
+      }
+
+      //Walk through the data from start to end
+      for (int i = startPoint - 1; i < endPoint; i++)
+      {
+        //computing a moving window of size period
+        double value = 0;
+        for (int j = i; j > i - period; j += -1)
+        {
+          value += data[j, columnID];
+        }
+        returnData[i] = value / period;
+      }
+      return returnData;
+    }
+  }
+
 }
 
--- CSharp/TSFLUnitTestProject/TSFLUnitTest.cs
+++ CSharp/TSFLUnitTestProject/TSFLUnitTest.cs
@@ -6,27 +6,51 @@ namespace TSFLUnitTestProject
   public class TSFLUnitTest
   {
     [TestMethod][TestCategory("MA")]
-    public void SMA_Simple_Test()
+    public void SMA_1D_Test()
     {
-      //Send a small array of same lenght as period
+      //Send a small array of same length as period
       var array = new double[] {1.0, 2.0, 3.0};
-      var result = TSFL.TSFLLib.SMA(array, 3);
+      
+      var result = TSFL.SMA.Compute(array, 3);
       //result should come back with a 2 in the last position (1+2+3)/3
       Assert.AreEqual(2.0, result[2]);
 
-      //Send a small array of same lenght as period
+      //Send a small array of same length as period
       array = new double[] { 1.0, 2.0, 3.0, 4.0, 5.0 };
-      result = TSFL.TSFLLib.SMA(array, 3);
+      result = TSFL.SMA.Compute(array, 3);
       //result should come back with a 4 in the last position (3+4+5)/3
       Assert.AreEqual(4.0, result[4]);
 
       //Call with additional start and end parameters set to beginning and end of array
-      result = TSFL.TSFLLib.SMA(array, 3, 0, array.Length);
+      result = TSFL.SMA.Compute(array, 3, 0, array.Length);
       Assert.AreEqual(4.0, result[4]);
 
-      //Call with intermediate range to compute
-      result = TSFL.TSFLLib.SMA(array, 3, 1, 3);
+      //Call with intermediate range to compute, assert on 2nd to last
+      result = TSFL.SMA.Compute(array, 3, 1, 3);
+      Assert.AreEqual(3.0, result[3]);
+    }
+    [TestMethod][TestCategory("MA")]
+    public void SMA_2D_Test()
+    {
+      //Send a small array of same length as period
+      var array = new double[,] { {1.0, 1.0}, {2.0, 2.0}, {3.0, 3.0} };
+      var result = TSFL.SMA.Compute(array, 3, 1);
+      //result should come back with a 2 in the last position (1+2+3)/3
+      Assert.AreEqual(2.0, result[2]);
+
+      //Send a small array of same length as period
+      array = new double[,] { { 1.0, 1.0 }, { 2.0, 2.0 }, { 3.0, 3.0 }, { 4.0, 4.0 }, { 5.0, 5.0 } };
+      result = TSFL.SMA.Compute(array, 3, 1);
+      //result should come back with a 4 in the last position (3+4+5)/3
       Assert.AreEqual(4.0, result[4]);
+
+      //Call with additional start and end parameters set to beginning and end of array
+      result = TSFL.SMA.Compute(array, 3, 0, array.Length, 1);
+      Assert.AreEqual(4.0, result[4]);
+
+      //Call with intermediate range to compute, assert on 2nd to last
+      result = TSFL.SMA.Compute(array, 3, 1, 3, 1);
+      Assert.AreEqual(3.0, result[3]);
     }
   }
 }
